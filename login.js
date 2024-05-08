c7b96a4eb9ca40e1f88ff8ca656eb59893939f28
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-05-07 20:52:54 -0400

    added html and css base

---
+++ login.js
@@ -0,0 +1,11 @@
+<script>
+function loginAsUser() {
+    // Add logic to redirect or handle login as a regular user
+    console.log("Logging in as user");
+}
+
+function loginAsPro() {
+    // Add logic to redirect or handle login as a professional user
+    console.log("Logging in as pro");
+}
+</script>
