a861a95a37004f7dea5563c60e816e9c95d85523
Carlos Ruiz <carlosruiz9221@gmail.com>
2024-05-08 12:32:44 -0400

    additons to server.js, package.json and connection.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,26 @@
+DROP DATABASE IF EXISTS login_db;
 CREATE DATABASE login_db;
-USE login_db;
-CREATE TABLE user (
-    userId INT AUTO_INCREMENT PRIMARY KEY,
-    userRoleId INT,
-    UserFName VARCHAR(50),
-    UserLName VARCHAR(50),
-    email VARCHAR(50),
-);
-CREATE TABLE userLogin (
-    userId INT AUTO_INCREMENT PRIMARY KEY,
-    loginName VARCHAR(50),
-    loginPassword VARCHAR(50)
+
+
+-- USE login_db;
+-- CREATE TABLE user (
+--     userId INT AUTO_INCREMENT PRIMARY KEY,
+--     userRoleId INT,
+--     UserFName VARCHAR(50),
+--     UserLName VARCHAR(50),
+--     email VARCHAR(50),
+-- );
+-- CREATE TABLE userLogin (
+--     userId INT AUTO_INCREMENT PRIMARY KEY,
+--     loginName VARCHAR(50),
+--     loginPassword VARCHAR(50)
     
-);
-CREATE TABLE userRole (
-    userRoleId INT AUTO_INCREMENT PRIMARY KEY,
-    roleName VARCHAR(50)
-);
-CREATE TABLE userLoginRole (
-    userId INT,
-    userRoleId INT
-);
+-- );
+-- CREATE TABLE userRole (
+--     userRoleId INT AUTO_INCREMENT PRIMARY KEY,
+--     roleName VARCHAR(50)
+-- );
+-- CREATE TABLE userLoginRole (
+--     userId INT,
+--     userRoleId INT
+-- );
